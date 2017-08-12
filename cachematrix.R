c716011a271d8399619807f5885e282c369b4484
kitkat <sneakekat@gmail.com>
2017-08-12 07:56:05 -0600

    prog. assigm. 2 cachematrixniverse

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeMatrix function creates inverse of square matrix
+## and stores the result in makeCacheMatrix function for easy
+## access
 
-## Write a short comment describing this function
+## Sets & gets value of matrix for easy access
+## setsinverse & gets inverse already stored
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {  #only runs if I reset vector after initialized
+    x <<- y     #sets x in parent environment  
+    m <<- NULL  #sets m in parent environment
+  }
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse #sets m in parent environment
+  getinverse <- function() m 
+  list(set=set, get=get, setinverse=setinverse, 
+       getinverse=getinverse)
+  
 }
 
+## Calculates Inverse of square matrix
+## Caches result in makeCacheMatrix function
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) {   #still not sure why you need ...
+  m <- x$getinverse()
+  if (!is.null(m)) {
+    message("getting cached inverse")
+    return(m)  #why do i need return? can't I just say m?
+  }
+  data <- x$get()
+  m <- solve(data, ...) # was is ... for?
+  x$setinverse(m)
+  m
 }
